db70963a6043be97cb349a969f6830d8c2a85dcf
jiyinyiyong <jiyinyiyong@gmail.com>
2019-01-23 01:44:24 +0800

    fix hot swapping; change logo

--- src/app/logo.cljs
+++ src/app/logo.cljs
@@ -5,27 +5,26 @@
   (PIXI/Application.
    (clj->js {:width 400, :height 400, :transparent true, :antialias true, :resolution 1})))
 
-(defn create-logo! [store]
-  (.appendChild (.querySelector js/document ".container") pixi-app.view)
+(defn draw-logo! []
   (dotimes [n 8]
     (let [g (PIXI/Graphics.)]
-      (-> g
-          (.beginFill 0xffee44)
-          (.lineStyle 0 0x8888ff 1)
-          (.drawEllipse 0 96 28 96)
-          (.endFill))
-      (-> g .-alpha (set! 0.6))
+      (-> g (.lineStyle 12 0x888899 1) (.drawEllipse 0 96 40 96))
+      (-> g .-alpha (set! 1))
+      (-> g .-transform .-pivot (.set 0 0))
+      (-> g .-position (.set 200 200))
+      (-> g .-rotation (set! (- (* n (/ js/Math.PI 4)) 0.1)))
+      (.addChild pixi-app.stage g))
+    (let [g (PIXI/Graphics.)]
+      (-> g (.lineStyle 16 0xffee77 1) (.drawEllipse 0 96 40 96))
+      (-> g .-alpha (set! 1))
       (-> g .-transform .-pivot (.set 0 0))
       (-> g .-position (.set 200 200))
       (-> g .-rotation (set! (* n (/ js/Math.PI 4))))
       (.addChild pixi-app.stage g))))
 
-(defn update-logo! [store]
-  (.log js/console pixi-app.stage)
-  (.reload js/location)
-  (dotimes [n 10]
-    (comment
-     let
-     ((graphics (PIXI.Graphics.)))
-     (-> graphics (.beginFill 0xff3300) (.drawEllipse 100 100 80 40) (.endFill))
-     (.addChild pixi-app.stage graphics))))
+(defn create-logo! [store]
+  (.appendChild (.querySelector js/document ".container") pixi-app.view)
+  (comment js/console.log pixi-app.stage pixi-app.view)
+  (draw-logo!))
+
+(defn update-logo! [store] (.removeChildren pixi-app.stage) (draw-logo!))
